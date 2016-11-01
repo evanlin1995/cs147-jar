e40be5a3829d9dee581e101041aca05349a355ee
Evan Lin <evanlin@stanford.edu>
2016-10-31 21:56:00 -0700

    Initial Commit

---
+++ Jar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jar
+//
+//  Created by Evan Lin on 10/31/16.
+//  Copyright (c) 2016 Evan Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
